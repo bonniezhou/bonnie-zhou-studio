00d61ec8a3841dba33533cc643c5ea9951485a77
Bonnie Zhou <bonniez@google.com>
2022-05-08 18:46:35 -0400

    typography

--- src/Home.js
+++ src/Home.js
@@ -9,10 +9,11 @@ function Home() {
     <div className='home'>
       <NavBar />
       <h3>
-        Artwork inspired by the expression, movement, and sensuality of the body
+        Artwork inspired by the movement, sensuality, and expression of the body
       </h3>
-      <h4>Sometimes we can spark like fire, but sometimes we're meant to flow like water. My art explores the ways we express ourselves through our bodies — the physical manifestation of our souls — and the innate connection we have to elements of nature.</h4>
+      <h4>Sometimes we spark like fire, but sometimes we're meant to flow like water. My art explores the ways we express ourselves through our bodies — the physical manifestation of our souls — and the innate connection we have to elements of nature.</h4>
       {Button('Shop the Collection')}
+      <p>In my art, I reconnect with our existence as human beings on Earth. No screens, no judgement, just our bodies and nature. I explore themes of ease and resistance, peace in chaos, and coming home to who we are. Imperfect wholeness. Physical manifestations. A beautiful organic mess.</p>
       <Email />
       <Footer />
     </div>
