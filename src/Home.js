915ce53b75ae902aa18b0be15e623f15bb2eeb15
Bonnie Zhou <bonniez@google.com>
2022-05-08 20:54:21 -0400

    Add header

--- src/Home.js
+++ src/Home.js
@@ -8,12 +8,13 @@ function Home() {
   return (
     <div className='home'>
       <NavBar />
+      <div className='header' />
       <h3>
         Artwork inspired by the movement, sensuality, and expression of the body
       </h3>
       <h4>Sometimes we spark like fire, but sometimes we're meant to flow like water. My art explores the ways we express ourselves through our bodies — the physical manifestation of our souls — and the innate connection we have to elements of nature.</h4>
       {Button('Shop the Collection')}
-      <p>In my art, I reconnect with our existence as human beings on Earth. No screens, no judgement, just our bodies and nature. I explore themes of ease and resistance, peace in chaos, and coming home to who we are. Imperfect wholeness. Physical manifestations. A beautiful organic mess.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sit amet lorem elementum, tincidunt urna vitae, vestibulum nunc. Curabitur a nisi iaculis arcu vestibulum fermentum eget ac odio. Mauris ultricies augue vitae mattis eleifend. Nullam at dapibus elit. Aliquam auctor nec magna ut sagittis. Aliquam condimentum placerat justo ornare malesuada. Curabitur id laoreet nibh. Praesent scelerisque elit vitae dolor imperdiet, in finibus erat varius.</p>
       <Email />
       <Footer />
     </div>
